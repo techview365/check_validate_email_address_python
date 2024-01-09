3ac920ef6100155de5122da1cc78dea46387c6e9
Manish <manish.dinkar13@gmail.com>
2024-01-09 19:51:29 +0530

    first commit

---
+++ gmail.py
@@ -0,0 +1,9 @@
+# a-z techview365@gmail.com
+# 0-9
+# . and _ comes only 1 time
+# @ only 1 time
+# .com only 1 time and it should come from back in 2nd position or 3rd position like
+# .com or .in
+
+import re
+email_condition = "^[a-z]+[\._]?"
